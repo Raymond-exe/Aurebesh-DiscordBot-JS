b8e24616001698bdc9eb185b151405c6fb9aac38
Raymond-exe <Raymond-exe@users.noreply.github.com>
2020-11-08 10:51:37 -0800

    Swaped .bmp file for .png

--- discordBot.js
+++ discordBot.js
@@ -25,18 +25,20 @@ client.on('message', (messageEvent) => {
 
             switch(command) {
                 case 'key':
-                    response = {files: ["https://4.bp.blogspot.com/_eHPoZlMooF4/SLzo0MdZXZI/AAAAAAAAAEc/eWHKMH4GY3w/w1200-h630-p-k-no-nu/The+Aurebesh.bmp"]};//'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.explicit.bing.net%2Fth%3Fid%3DOIP.Y1Xg_WAVI6L2KZr4PrLqcgHaGE%26pid%3DApi&f=1';
+                    response = {files: ["https://ootinicast.com/aurebesh/Aurebesh.png"]};
+                    //alt link:
+                    //'https://4.bp.blogspot.com/_eHPoZlMooF4/SLzo0MdZXZI/AAAAAAAAAEc/eWHKMH4GY3w/w1200-h630-p-k-no-nu/The+Aurebesh.bmp';
                     break;
                 case 'help':
                     response = '**This is a temporary menu. Remind me to fix it lol.**\nCommands: `~help`, `~key`'; //TODO make the help command
                     break;
                 case 'version':
-                    response = 'version 201108.0.1'
+                    response = 'version 201108.0.3'
                     break;
                 default:
                     response = 'Command not \"' + CMD_PREFIX + command + '\"recognized. Use ' + CMD_PREFIX + 'help to see all commands!';
             }
-            
+
             messageEvent.channel.send(response);
 
             return
