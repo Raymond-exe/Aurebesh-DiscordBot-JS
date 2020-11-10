946388cd39c3b3243cb5a3c832a7b0add34fd4de
Raymond-exe <Raymond-exe@users.noreply.github.com>
2020-11-09 18:22:54 -0800

    Update discordBot.js

--- discordBot.js
+++ discordBot.js
@@ -49,7 +49,7 @@ function onMessageReceived(messageEvent) {
                     quoteCmd(messageEvent.channel)
                     break;
                 case 'version':
-                    response = 'version ID 201109.0 TEST' //TODO find a better way to do this lol
+                    response = 'version ID 201109.1 TEST' //TODO find a better way to do this lol
                     break;
                 default:
                     response = 'Command \"' + CMD_PREFIX + command + '\" not recognized. Use ' + CMD_PREFIX + 'help to see all commands!';
@@ -91,14 +91,10 @@ function helpCmd(channel) {
 
 function quoteCmd(channel) {
     var quotes = require("./quotes.json");
-    /*
-    var quotes = JSON.parse(localStorage.getItem('quotes.json')).quotes
     var selectedIndex = Math.random()*quotes.length
     var selectedQuote = quotes[selectedIndex]
 
     channel.send("\"" + selectedQuote.text + "\" " + (selectedQuote.author=="" ? "" : "-"+selectedQuote.author))
-    //*/
-    channel.send(quotes.quotes);
 }
 
 function getTranslationLink(str) {
